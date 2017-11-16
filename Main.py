e445dbb2ccfc563230b78b20c0b488a783c778f6
Samuel Terra <samuelterra@celulaweb.com.br>
2017-11-16 16:28:28 -0200

    Metodo para criar categorias criado.

--- Main.py
+++ Main.py
@@ -13,8 +13,19 @@ joomla_config = {
 joomla_util = JoomlaUtil(config=joomla_config)
 
 # print(len(joomla_util.import_posts_k2()))
-# print(len(joomla_util.import_categories_k2()))
 # print(joomla_util.remove_all_posts())
+# categories = joomla_util.import_categories_k2()
+#
+#
+# sorted_categories = sorted(categories, key=lambda category: category.get_parent())   # sort by parent
+#
+#
+# # print(sorted_categories)
+# for cat in sorted_categories:
+#     print(cat.get_id())
+
+
+
 
 
 wp_config = {
@@ -22,10 +33,19 @@ wp_config = {
     "db_wp_user": "root",
     "db_wp_pass": "",
     "db_wp_name": "corrego_teste_wp",
-    "db_table_prefix": "wp_"
+    "db_table_prefix": "wp_",
+
+    "site_url": "http://localhost/corrego_teste",
+    "site_password": "*0109lr*",
+    "site_username": "suporte",
+
+    # "site_url": "http://local.corrego",
+    # "site_password": "#cs0117sw#"
 }
 
 wp_util = WordpressUtil(config=wp_config)
 
-# print(wp_util.remove_all_posts())
-# print(wp_util.remove_all_categories())
+# wp_util.remove_all_posts()
+# wp_util.remove_all_categories()
+# wp_util.post_to_wp(None)
+wp_util.post_category(None)
--- Util/WordpressUtil.py
+++ Util/WordpressUtil.py
@@ -1,14 +1,11 @@
 #  http://python-wordpress-xmlrpc.readthedocs.io/en/latest/overview.html
-
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
-from wordpress_xmlrpc.compat import xmlrpc_client
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.compat import xmlrpc_client
-from wordpress_xmlrpc.methods import media, posts
-from wordpress_xmlrpc.methods.users import GetUserInfo
+from xmlrpc import client
 
 import MySQLdb
+from wordpress_xmlrpc import xmlrpc_client, WordPressTerm
+from wordpress_xmlrpc.methods import media, taxonomies
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods.posts import NewPost
 
 
 class WordpressUtil(object):
@@ -19,6 +16,9 @@ class WordpressUtil(object):
         self._db_wp_name = config["db_wp_name"]
 
         self._db_table_prefix = config["db_table_prefix"]
+        self._site_url = config["site_url"]
+        self._site_username = config["site_username"]
+        self._site_password = config["site_password"]
 
     def get_connection(self):
         return MySQLdb.connect(
@@ -45,12 +45,16 @@ class WordpressUtil(object):
 
     def remove_all_categories(self):
         db = self.get_connection()
+        cur = db.cursor()
 
         table = str(self._db_table_prefix) + "term_taxonomy"
-
-        cur = db.cursor()
         cur.execute("DELETE FROM " + table + " WHERE term_taxonomy_id != 1 AND taxonomy = 'category'")
         db.commit()
+
+        table = str(self._db_table_prefix) + "terms"
+        cur.execute("DELETE FROM " + table + " WHERE term_id != 1 AND slug != 'sem-categoria'")
+        db.commit()
+
         result = cur.fetchall()
 
         db.close()
@@ -71,37 +75,58 @@ class WordpressUtil(object):
 
         return result
 
-# client = Client('http://localhost/corrego_teste/xmlrpc.php', 'suporte', '*0109lr*')
-#
-# # set to the path to your file
-# # filename = 'imagens/' + md5('Image' + id_post_joomla) + '.jpg'
-# filename = 'b414e2ba4719c9b4ceec2e3c09e2491b.jpg'
-#
-# # prepare metadata
-# data = {
-#     'name': 'picture.jpg',
-#     'type': 'image/jpeg',  # mimetype
-# }
-#
-# # read the binary file and let the XMLRPC library encode it into base64
-# with open(filename, 'rb') as img:
-#     data['bits'] = xmlrpc_client.Binary(img.read())
-#
-# response = client.call(media.UploadFile(data))
-# # response == {
-# #       'id': 6,
-# #       'file': 'picture.jpg'
-# #       'url': 'http://www.example.com/wp-content/uploads/2012/04/16/picture.jpg',
-# #       'type': 'image/jpeg',
-# # }
-# attachment_id = response['id']
-#
-# post = WordPressPost()
-# post.title = 'My new title'
-# post.content = 'This is the body of my new post.'
-# post.thumbnail = attachment_id
-# post.terms_names = {
-#     'post_tag': ['test', 'firstpost'],
-#     'category': ['Introductions', 'Tests']
-# }
-# client.call(NewPost(post))
+    def post_thumbnail(self, client):
+        # set to the path to your file
+        # filename = 'imagens/' + md5('Image' + id_post_joomla) + '.jpg'
+        filename = 'b414e2ba4719c9b4ceec2e3c09e2491b.jpg'
+
+        # prepare metadata
+        data = {
+            'name': 'picture.jpg',
+            'type': 'image/jpeg',  # mimetype
+        }
+
+        # read the binary file and let the XMLRPC library encode it into base64
+        with open(filename, 'rb') as img:
+            data['bits'] = xmlrpc_client.Binary(img.read())
+
+        response = client.call(media.UploadFile(data))
+        # response == {
+        #       'id': 6,
+        #       'file': 'picture.jpg'
+        #       'url': 'http://www.example.com/wp-content/uploads/2012/04/16/picture.jpg',
+        #       'type': 'image/jpeg',
+        # }
+        return response
+
+    def post_category(self, category):
+        client = Client(str(self._site_url) + '/xmlrpc.php', self._site_username, self._site_password)
+
+        cat = WordPressTerm()
+        cat.taxonomy = 'category'
+        cat.parent = 1  # id da categoria pai
+        cat.name = 'category bla'
+        cat.id = client.call(taxonomies.NewTerm(cat))
+
+        return cat
+
+    def post_categories(self, categories):
+        sorted_categories = sorted(categories, key=lambda category: category.get_parent())  # sort by parent
+
+        for category in categories:
+            self.post_category(category)
+
+    def post_to_wp(self, wp_post):
+        client = Client(str(self._site_url) + '/xmlrpc.php', self._site_username, self._site_password)
+        post = WordPressPost()
+        post.title = 'My new title'
+        post.content = 'This is the body of my new post.'
+        post.thumbnail = self.post_thumbnail(client)['id']
+        post.post_status = "publish"
+        post.terms_names = {
+            # 'post_tag': ['test', 'firstpost'],
+            'category': ['Introductions', 'Tests']
+        }
+        client.call(NewPost(post))
+
+        return post
