fec11982cb6a46862cbd710b7620c201b69e5b54
Samuel Terra <samuelterra22@hotmail.com>
2018-01-11 15:59:30 -0200

    Script de instalação de dependencias atualizado.

--- install.sh
+++ install.sh
@@ -6,9 +6,23 @@
 # export LC_ALL="en_US.UTF-8"
 # export LC_CTYPE="en_US.UTF-8"
 # sudo dpkg-reconfigure locales
+echo "###############################################################"
+echo "# Instalação das dependências do projeto                      #"
+echo "###############################################################"
+echo "Caso de erro de locals, executar os seguintes comandos:"
+echo " $ export LC_ALL=\"en_US.UTF-8\""
+echo " $ export LC_CTYPE=\"en_US.UTF-8\""
+echo " $ sudo dpkg-reconfigure locales"
+echo "###############################################################"
 
-sudo python3.5 pip install python-wordpress-xmlrpc
+sudo apt-get install python3-pip
+sudo python3.5 -m pip install --upgrade pip
+sudo apt-get install python3-mysqldb
+sudo python3.5 pip -m install python-wordpress-xmlrpc
 sudo python3.5 -m pip install mysqlclient
-sudo apt-get install libssl-dev python3-setuptools
+sudo apt-get install -y libssl-dev python3-setuptools
 sudo python3.5 -m pip install progressbar2
 sudo python3.5 -m pip install numpy
+
+echo "###############################################################"
+echo "Fim da instalação das dependências."
